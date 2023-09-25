e985b6cba227bbc1797f11fc7a9d879de2b95128
Luis Fernando González Garay <luis95.garay@gmail.com>
2023-09-24 21:44:04 -0600

    remove generator part

--- backend/src/data_processing_api/routes/__init__.py
+++ backend/src/data_processing_api/routes/__init__.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 
-from . import (
-    data_preprocessing, text_extraction
-)
+from . import text_extraction
 
 
 def route_registry(app: FastAPI):
     app.include_router(text_extraction.router)
-    app.include_router(data_preprocessing.router)
